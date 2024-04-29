6aa24fce17f8eff25c7889adaec70b7411537dff
Aleenaprince <princessaleena2002@gmail.com>
2024-04-30 03:59:23 +0530

    alert button added

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 const db = mysql.createConnection({
     host: "localhost",
     user: 'root',
-    password: "password",
+    password: "Al2711eena!",
     database: "edms",
 });
 db.connect((err) => {
